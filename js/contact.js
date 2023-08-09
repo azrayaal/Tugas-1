b4818a0cf986802ae5ef639efeb811071de8ec8e
azra <azrayazidalkautsar@gmail.com>
2023-08-09 13:38:19 +0700

    update js function form contact

--- js/contact.js
+++ js/contact.js
@@ -4,16 +4,19 @@ function submitData() {
     let phone = document.getElementById("input-phone").value;
     let subject = document.getElementById("input-subject").value;
     let message = document.getElementById("input-message").value;
+
     
-    if (name == "") {
+
+    
+    if (name === "") {
     return(alert('nama harus di sisi'))
-    } else if (email == ""){
+    } else if (email === ""){
     return(alert('email harus di sisi'))
-    } else if (phone == ""){
+    } else if (phone === ""){
     return(alert('phone harus di sisi'))
-    } else if (subject == ""){
+    } else if (subject === ""){
     return(alert('subject harus di sisi'))
-    } else if (message == ""){
+    } else if (message === ""){
     return(alert('message harus di sisi'))
     } else {
    
@@ -21,7 +24,6 @@ function submitData() {
 
         let kirim = document.createElement('a');
         kirim.href = `https://mail.google.com/mail/?view=cm&fs=1&to=${emailReceiver}&su=${subject}&body=${message}`
-        
         kirim.click()
 
         let data = {name, email, phone, subject, message};
