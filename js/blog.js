5046223db7a749a5942d6cb3e71afe89a7ce75ee
azra <azrayazidalkautsar@gmail.com>
2023-08-11 00:19:41 +0700

    update blog

--- js/blog.js
+++ js/blog.js
@@ -39,29 +39,28 @@ function renderBlog() {
     console.log("inidatarenderblog", dataBlog[index]);
 
     document.getElementById("content").innerHTML += `
-    <div id="content" class="blog-list">
-    <div class="blog-list-item">
-      <div class="blog-image">
-        <div class="img">
-          <img class="img-blog" src="${dataBlog[0].image}" alt="" />
-        </div>
-      </div>
-      <div class="blog-content">
-        <div class="blog-letter">
-          <a class="blog-title" href="blog-detail.html" target="_blank"
-            >${dataBlog[0].title}</a
-          >
-          <div class="detail-blog-content">Technologies</div>
-          <p>
-          ${dataBlog[0].content}
-          </p>
-        </div>
-        <div class="button-group">
-          <button class="btn-edit">Edit Post</button>
-          <button class="btn-delete">Delete Post</button>
-        </div>
-      </div>
-    </div>
-  </div>`;
+
+        <div class="blog-list-item">
+          <div class="blog-image">
+            <div class="img">
+              <img class="img-blog" src="${dataBlog[0].image}" alt="" />
+            </div>
+          </div>
+          <div class="blog-content">
+            <div class="blog-letter">
+              <a class="blog-title" href="blog-detail.html" target="_blank"
+                >${dataBlog[0].title}</a
+              >
+              <div class="detail-blog-content">Technologies</div>
+              <p>
+              ${dataBlog[0].content}
+              </p>
+            </div>
+            <div class="button-group">
+              <button class="btn-edit">Edit Post</button>
+              <button class="btn-delete">Delete Post</button>
+            </div>
+          </div>
+      </div>`;
   }
 }
