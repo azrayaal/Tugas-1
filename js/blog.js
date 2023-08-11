60b027113790ef94f7d6c0fe8366757219a67fc0
azra <azrayazidalkautsar@gmail.com>
2023-08-11 17:57:32 +0700

    update distance time blog

--- js/blog.js
+++ js/blog.js
@@ -5,63 +5,52 @@ function addBlog(event) {
 
   let title = document.getElementById("input-blog-title").value;
   let content = document.getElementById("input-blog-content").value;
+  // DATE //////////////////////////////////////
   let startDate = document.getElementById("input-start-date").value;
   let endDate = document.getElementById("input-end-date").value;
-  let image = document.getElementById("input-blog-image").files;
-  image = URL.createObjectURL(image[0]);
-  // masukin data
 
-  // technologies
-  let technologies = [];
-  document.querySelectorAll('[type="checkbox"]').forEach((item) => {
-    if (item.checked === true) {
-      technologies.push(item.value);
-    }
-  });
+  // IMAGE //////////////////////////////////////
 
-  // duration
-  const date1 = new Date(startDate);
-  const date2 = new Date(endDate);
-  const time = Math.abs(date2 - date1);
-  const days = Math.ceil(time / (1000 * 60 * 60 * 24));
-  let duration = days;
+  // let image = document.getElementById("input-blog-image").files;
+  // image = URL.createObjectURL(image[0]);
+
+  // TECHNOLOGIES /////////////////////////////
+
+  // let technologies = [];
+  // document.querySelectorAll('[type="checkbox"]').forEach((item) => {
+  //   if (item.checked === true) {
+  //     technologies.push(item.value);
+  //   }
+  // });
+
+  // DURATION ///////////////////////////////////
+
+  // const date1 = new Date(startDate);
+  // const date2 = new Date(endDate);
+  // const time = date2 - date1;
+  // const days = Math.ceil(time / (1000 * 60 * 60 * 24));
+  // let duration = days;
+
+  /////////////////////////////////////////////
+
+  /////////////////////////
 
   let blog = {
     title,
     content,
-    startDate,
-    endDate,
-    duration,
-    image,
-    technologies,
-    postAt: "augst",
+    // startDate,
+    // endDate,
+    // duration,
+    // image,
+    // technologies,
+    postAt: new Date(),
     author: "Azra Yazid",
   };
 
   dataBlog.push(blog);
-
   console.log("ini datablog dari addblog", dataBlog);
 
   renderBlog();
-}
-
-// function render blog
-
-function getItemdrclick() {
-  let title = document.getElementById("input-blog-title").value;
-  let content = document.getElementById("input-blog-content").value;
-
-  let datadata = {
-    title,
-    content,
-  };
-
-  let databuatsimpen = JSON.stringify(datadata);
-  localStorage.setItem("datasimpenanBlg", databuatsimpen);
-
-  let dapetdata = JSON.parse(localStorage.getItem("datasimpenanBlg"));
-
-  console.log("dapet nih?", dapetdata);
 }
 
 function renderBlog() {
@@ -71,7 +60,7 @@ function renderBlog() {
     console.log("inidatarenderblog", dataBlog[index]);
 
     document.getElementById("content").innerHTML += `
-
+    
         <div class="blog-list-item">
           <div class="blog-image">
             <div class="img">
@@ -81,13 +70,21 @@ function renderBlog() {
           <div class="blog-content">
             <div class="blog-letter">
               <a onclick="getItemdrclick()" class="blog-title" href="blog-detail.html" target="_blank"
-                >${dataBlog[index].title}</a
+                >    ${getRealTime(dataBlog[index].postAt)}
+                ||${dataBlog[index].title}
+                </a
               >
-              <div class="detail-blog-content">${dataBlog[index].technologies}</div>
+              <div class="detail-blog-content">${
+                dataBlog[index].technologies
+              }</div>
               <div class="duration">${dataBlog[index].duration} Days</div>
               <p>
-              ${dataBlog[index].content}
+              
+              ${dataBlog[index].content} || <br>
+              ${getDistanceTime(dataBlog[index].postAt)}
+           
               </p>
+
             </div>
             <div class="button-group">
               <button class="btn-edit">Edit Post</button>
@@ -98,4 +95,62 @@ function renderBlog() {
   }
 }
 
-// function getItem
+function getRealTime(time) {
+  let namaBulan = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Des",
+  ];
+
+  // let realTime = time();
+  let date = time.getDate();
+  let month = time.getMonth();
+  let year = time.getFullYear();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+
+  // if (hours <= 9) {
+  //   // 09
+  //   hours = "0" + hours;
+  // } else if (minutes <= 9) {
+  //   minutes = "0" + minutes;
+  // }
+
+  // return `${date} ${namaBulan[month]} ${year} || ${hours}:${minutes}`;
+  console.log(`${date} ${namaBulan[month]} ${year} ${hours}:${minutes} WIB`);
+  return `${date} ${namaBulan[month]} ${year} ${hours}:${minutes} WIB`;
+}
+
+function getDistanceTime(time) {
+  let timeNow = new Date();
+  let timePost = time;
+  let distanceTime = timeNow - timePost;
+
+  let distanceDay = Math.floor(distanceTime / (1000 * 3600 * 24));
+  let distanceHour = Math.floor(distanceTime / (1000 * 60 * 60));
+  let distanceMinute = Math.floor(distanceTime / (1000 * 60));
+  let distanceSecond = Math.floor(distanceTime / 1000);
+
+  if (distanceDay > 0) {
+    return `${distanceDay} days ago`;
+  } else if (distanceHour > 0) {
+    return `${distanceHour} hour ago`;
+  } else if (distanceMinute > 0) {
+    return `${distanceMinute} minute ago`;
+  } else if (distanceSecond > 0) {
+    return `${distanceSecond} second ago`;
+  }
+}
+
+setInterval(function () {
+  renderBlog();
+}, 3000);
