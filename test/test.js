ee81f1d4e0c445bd19b205f546773aefa8315a9e
azra <azrayazidalkautsar@gmail.com>
2023-08-16 14:38:04 +0700

    update testimonial page

--- test/test.js
+++ test/test.js
@@ -22,13 +22,60 @@ technologies.push("second");
 //   }
 // }
 
-technologies.forEach((currValue) => {
-  switch (currValue) {
-    case "first":
-      console.log("This is first");
-      break;
-    case "second":
-      console.log("This is second");
-      break;
+// technologies.forEach((currValue) => {
+//   switch (currValue) {
+//     case "first":
+//       console.log("This is first");
+//       break;
+//     case "second":
+//       console.log("This is second");
+//       break;
+//   }
+// });
+
+// let cv = false;
+
+// function clickbuton() {
+//   let downloadCv = document.getElementById("download-cv");
+//   if (!cv) {
+//     downloadCv.style.display = "none";
+//     cv = true;
+//     alert("haha");
+//   } else {
+//   }
+// }
+
+class Car {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
   }
-});
+
+  getInfo() {
+    return `the car is a ${this.make} ${this.model}`;
+  }
+}
+
+// let car1 = new Car("Toyota", "Supra");
+
+// console.log(car1.getInfo());
+
+// inheritance
+
+class ElectricCar extends Car {
+  constructor(make, model, batteryCapacity) {
+    super(make, model);
+    this.batteryCapacity = batteryCapacity;
+  }
+
+  getInfo() {
+    return `${super.getInfo()} it has a battery ${this.batteryCapacity} KWH`;
+  }
+}
+
+let myElectric = new ElectricCar("tesla", "roster", "100");
+
+console.log(myElectric.getInfo());
+
+// polymorphism dapat membuat beberapa metode atau fungsi dengan nama yang sama, namun memiliki parameter yang berbeda.
+// abstaction
